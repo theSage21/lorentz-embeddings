factor code fix gradient accumulation

lorentz.py
@@ -3,7 +3,8 @@ import random
 import numpy as np
 from torch import nn
 from torch import optim
-# import matplotlib.pyplot as plt
+from tqdm import trange, tqdm
+from torch.utils.data import Dataset, DataLoader
 
 
 def arcosh(x):
@@ -29,6 +30,15 @@ def exp_map(x, v):
     result = torch.cosh(tn) * x + torch.sinh(tn) * (v / tn)
     result = torch.where(tn_expand > 0, result, x)  # only update if tangent norm is > 0
     return result
+
+
+def set_dim0(x):
+    dim0 = torch.sqrt(1 + torch.norm(x[:, 1:], dim=1))
+    x[:, 0] = dim0
+    return x
+
+
+# ========================= models
 
 
 class RSGD(optim.Optimizer):
@@ -58,8 +68,7 @@ class RSGD(optim.Optimizer):
                 update = exp_map(p, -group["learning_rate"] * proj)
                 is_nan_inf = torch.isnan(update) | torch.isinf(update)
                 update = torch.where(is_nan_inf, p, update)
-                dim0 = torch.sqrt(1 + torch.norm(update[:, 1:], dim=1))
-                update[:, 0] = dim0
+                update = set_dim0(update)
                 p.data.copy_(update)
 
 
@@ -76,8 +85,7 @@ class Lorentz(nn.Module):
         nn.init.uniform_(self.table.weight, -init_range, init_range)
         # equation 6
         with torch.no_grad():
-            dim0 = torch.sqrt(1 + torch.norm(self.table.weight[:, 1:], dim=1))
-            self.table.weight[:, 0] = dim0
+            set_dim0(self.table.weight)
             # self.table.weight[0] = 0  # padding idx
 
     def forward(self, I, Ks):
@@ -117,50 +125,87 @@ class Lorentz(nn.Module):
         # ---------- turn back to per-sample shape
         dists = dists.reshape(B, N)
         loss = -(dists[:, 0] - torch.log(torch.exp(dists).sum(dim=1) + 1e-6))
-        return loss, self.table.weight.data.numpy()
+        return loss
+
+    def lorentz_to_poincare(self):
+        table = self.table.weight.data.numpy()
+        return table[:, 1:] / (
+            table[:, :1] + 1
+        )  # diffeomorphism transform to poincare ball
 
 
-def lorentz_to_poincare(table):
-    return table[:, 1:] / (
-        table[:, :1] + 1
-    )  # diffeomorphism transform to poincare ball
+class Graph(Dataset):
+    def __init__(self, pairwise_matrix, sample_size=10):
+        self.pairwise_matrix = pairwise_matrix
+        self.n_items = len(pairwise_matrix)
+        self.sample_size = sample_size
 
+    def __len__(self):
+        return self.n_items
 
-def N_sample(matrix, i, j, n):
-    """
-    - Matrix    : is a simple pairwise similarity matrix
-    - i         : primary document
-    - j         : secondary document
-    - n         : Sample n items maximum from the matrix
-
-    0 is a padding index
-    """
-    min = matrix[i, j]
-    indices = [
-        index for index, is_less in enumerate(self.sim_matrix[i] < min) if is_less
-    ][:n]
-    return ([i + 1 for i in [j] + indices] + [0] * n)[:n]
+    def __getitem__(self, i):
+        I = torch.Tensor([i]).long()
+        while True:
+            j = random.randint(0, self.n_items - 1)
+            if j != i:
+                break
+        min = self.pairwise_matrix[i, j]
+        indices = [
+            index
+            for index, is_less in enumerate(self.pairwise_matrix[i] < min)
+            if is_less
+        ][: self.sample_size]
+        # offset indices by 1 and pad with 0
+        Ks = ([i + 1 for i in [j] + indices] + [0] * self.sample_size)[
+            : self.sample_size
+        ]
+        return I, torch.Tensor(Ks).long()
 
 
 if __name__ == "__main__":
-    emb_dim = 2
-    net = Lorentz(10, emb_dim + 1)  # as the paper follows R^(n+1) for this space
-    r = RSGD(net.parameters(), learning_rate=0.1)
+    import argparse
 
-    I = torch.Tensor([1, 2, 3, 4]).long()
-    Ks = torch.Tensor([[2, 3, 4, 9], [4, 5, 6, 1], [6, 7, 1, 5], [8, 9, 2, 1]]).long()
-    for i in range(4000):
-        loss, table = net(I, Ks)
-        loss = loss.mean()
-        loss.backward()
-        print(loss)
-        if torch.isnan(loss) or torch.isinf(loss):
-            break
-        r.step()
-    table = lorentz_to_poincare(table)
-    fig, ax = plt.subplots()
-    ax.scatter(*zip(*table))
-    for i, crd in enumerate(table):
-        ax.annotate(i, (crd[0], crd[1]))
-    plt.scatter(*zip(*table))
-    plt.show()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset", help="File:pairwise_matrix")
+    parser.add_argument(
+        "-sample_size", help="How many samples in the N matrix", default=10
+    )
+    parser.add_argument("-batch_size", help="How many samples in the batch", default=32)
+    parser.add_argument(
+        "-shuffle", help="Shuffle within batch while learning?", default=True
+    )
+    parser.add_argument(
+        "-epochs", help="How many epochs to optimize for?", default=1_000_000
+    )
+    parser.add_argument(
+        "-poincare_dim", help="Poincare projection time. Lorentz will be + 1", default=2
+    )
+    parser.add_argument(
+        "-n_items", help="How many items to embed?", default=None, type=int
+    )
+    parser.add_argument("-learning_rate", help="RSGD learning rate", default=0.1)
+    args = parser.parse_args()
+    fl, obj = args.dataset.split(":")
+
+    exec(f"from {fl} import {obj} as pairwise")
+    args.n_items = len(pairwise) if args.n_items is None else args.n_items
+    pairwise = pairwise[: args.n_items, : args.n_items]
+
+    dataloader = DataLoader(Graph(pairwise, args.sample_size), shuffle=args.shuffle)
+    net = Lorentz(
+        args.n_items, args.poincare_dim + 1
+    )  # as the paper follows R^(n+1) for this space
+    rsgd = RSGD(net.parameters(), learning_rate=args.learning_rate)
+
+    for i in trange(args.epochs, desc="Epochs"):
+        with tqdm(ncols=80) as pbar:
+            for I, Ks in tqdm(dataloader):
+                rsgd.zero_grad()
+                loss, table = net(I, Ks)
+                loss = loss.mean()
+                loss.backward()
+                rsgd.step()
+                pbar.set_description(f"Batch Loss: {float(loss)}")
+                if torch.isnan(loss) or torch.isinf(loss):
+                    break
+            table = net.lorentz_to_poincare()